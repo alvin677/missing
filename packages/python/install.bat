c1a335b500a27aff442a66ee3353567abe8c0cba
GoatCedric <112005397+GoatCedric@users.noreply.github.com>
2024-07-08 14:05:01 +0200

    gg

--- packages/python/install.bat
+++ packages/python/install.bat
@@ -1,12 +1,3 @@
-@echo off
 
-for /f "delims=" %%I in ('python --version 3^>^&1') do set "version=%%I"
-
-if defined version (
-    echo [42m[30m%version% is already installed.[0m[0m
-    where python
-    echo.
-) else (
-    curl -o C:/Users/%username%/.missing/python-installer.exe https://www.python.org/ftp/python/{VERSION}/python-{VERSION}-amd64.exe
-    C:/Users/%username%/.missing/python-installer.exe
-)
+curl -o C:/Users/%username%/.missing/python-installer.exe https://www.python.org/ftp/python/{VERSION}/python-{VERSION}-amd64.exe
+C:/Users/%username%/.missing/python-installer.exe
