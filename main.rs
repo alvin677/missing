07f57b46d345e6ffcdabba51f853e1f5cbdb277c
GoatCedric <112005397+GoatCedric@users.noreply.github.com>
2024-07-08 15:01:02 +0200

    gg

---
+++ main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // make directories; C:/Users/%username%/.missing, C:/Users/%username%/.missing/ins, C:/Users/%username%/.missing/bin
+    // set path to binaries: set PATH=%PATH%;C:/Users/%username%/.missing/bin
+}
