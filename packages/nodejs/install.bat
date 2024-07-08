3b8199b0c1f4ad1f22797c2aebe033c6092555bb
GoatCedric <112005397+GoatCedric@users.noreply.github.com>
2024-07-08 14:09:08 +0200

    Update install.bat

--- packages/nodejs/install.bat
+++ packages/nodejs/install.bat
@@ -1,2 +1,2 @@
-curl -o C:/Users/%username%/.missing/nodejs-installer.exe https://nodejs.org/dist/v{VERSION}/node-v{VERSION}-x64.msi
-C:/Users/%username%/.missing/nodejs-installer.exe
+curl -o C:/Users/%username%/.missing/nodejs-installer.msi https://nodejs.org/dist/v{VERSION}/node-v{VERSION}-x64.msi
+C:/Users/%username%/.missing/nodejs-installer.msi
