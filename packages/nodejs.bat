d957e3f05a1184863aa0278922b9154248d86b3a
GoatCedric <112005397+GoatCedric@users.noreply.github.com>
2024-07-08 14:15:59 +0200

    gg

---
+++ packages/nodejs.bat
@@ -0,0 +1,2 @@
+curl -o C:/Users/%username%/.missing/nodejs-installer.exe https://nodejs.org/dist/v{VERSION}/node-v{VERSION}-x64.msi
+C:/Users/%username%/.missing/nodejs-installer.exe
--- packages/python.bat
+++ packages/python.bat
@@ -1,3 +1,2 @@
-
 curl -o C:/Users/%username%/.missing/python-installer.exe https://www.python.org/ftp/python/{VERSION}/python-{VERSION}-amd64.exe
 C:/Users/%username%/.missing/python-installer.exe
